7057dd4095671ed92cb978428c3f38bb81d6e01c
Madison Wozniak <madisontgw@gmail.com>
2024-04-11 15:01:08 -0600

    mostly done

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # streamlit_app
+
+App description
+
+User instructions
+
+Clear explanation of interactive elements
--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from geopy.geocoders import Nominatim
 from uszipcode import SearchEngine
 from geopy.exc import GeocoderTimedOut
 
-st.title('Data Curation App')
+st.title('Los Angeles Job Explorer')
 
 data = pd.read_csv('3-26.csv')
 
@@ -21,12 +21,29 @@ with st.expander(label = 'Data Description'):
              this process, to gain a better understanding of the job 
              market in LA.""")
 
+# sidebar with description of this app
+with st.sidebar:
+    st.header('Purpose of this App:')
+    st.write("""
+             This app was created to help students or anyone pursuing Data Science
+              in the Los Angeles area, understand the current job market. 
+             The app can help answer questions such as "What cities in LA can I expect to work in?" or
+              "What is a realistic pay range for my experience?". 
+             
+             App Usage:\n
+             The user has the ability to enter a chosen salary range to view a list of 
+             both companies and titles that offer positions in that salary range. This app
+              also includes an interactive map of Los Angeles, which marks all the cities containing 
+             Data Science job postings. 
+             """)
 
 # map of all the locations in my df
-st.title('Map of Cities in LA County That Offer Data Science Positions')
-map_sc = folium.Map(location=[34.052235, -118.243683], zoom_start=7)
+st.header('Question 1: What cities in LA County have positions available?')
+st.subheader('Map of Cities in LA County That Offer Data Science Positions')
+map_sc = folium.Map(location=[34.052235, -118.243683], zoom_start=9)
 geolocator = Nominatim(user_agent="southern_california")
 cities = data['City']
+# dictionary of common cities to save time
 la_city_coordinates = {
     'Los Angeles': (34.052235, -118.243683),
     'Long Beach': (33.770050, -118.193740),
@@ -58,17 +75,65 @@ for city in cities:
         if city in la_city_coordinates:
             folium.Marker(location=la_city_coordinates[city], popup=city).add_to(map_sc)
         else:
-            #print(f'city: {city}')
             location = geolocator.geocode(city + ', Southern California', timeout=10)
             if location:
                 folium.Marker(location=[location.latitude, location.longitude], popup=city).add_to(map_sc)
 map_html = map_sc._repr_html_()
-st.components.v1.html(map_html, width = 800, height=600)
-
-# sort by salary and show distribution of highest paid roles
-# to demonstrate what is possible as a data scientist
+st.components.v1.html(map_html, width = 800, height=500)
     
 # How do your options change depending on work environment
     
 
+# choose company to see what jobs they're hiring for
 
+# salary list to see which companies/jobs have that pay
+st.header('Question 2: What Salary can I expect for a given job?')
+st.write('Below will appear a list of jobs and companies for the given range')
+popover = st.popover("Choose a Salary Range")
+low = popover.checkbox('Show \$0 - \$50,000', False)
+med = popover.checkbox('Show \$50,000  - \$100,000', False)
+med2 = popover.checkbox('Show \$100,000 - \$350,000 ', False)
+high = popover.checkbox('Show \$350,000 + ', False)
+
+if low:
+    st.subheader('Companies and Jobs with Salary Range \$0 - \$50,000')
+    low_df = data[data['Pay'] <= 50000]
+    jobs1 = low_df['Title']
+    companies1 = low_df['Company']
+    for job, company in zip(jobs1, companies1):
+        st.write(f'{company}: {job}')  
+if med:
+    st.subheader('Companies and Jobs with Salary Range \$50,000  - \$100,000')
+    med_df = data[(data['Pay'] >= 50000) & (data['Pay'] <= 100000)]
+    jobs2 = med_df['Title']
+    companies2 = med_df['Company']
+    for job, company in zip(jobs2, companies2):
+        st.write(f'{company}: {job}')
+if med2:
+    st.subheader('Companies and Jobs with Salary Range \$100,000 - \$350,000 ')
+    med_df2 = data[(data['Pay'] >= 100000) & (data['Pay'] <= 350000)]
+    jobs3 = med_df2['Title']
+    companies3 = med_df2['Company']
+    for job, company in zip(jobs3, companies3):
+        st.write(f'{company}: {job}')
+if high:
+    st.subheader('Companies and Jobs with Salary Range \$350,000 +')
+    high_df = data[data['Pay'] >= 350000]
+    jobs4 = high_df['Title']
+    companies4 = high_df['Company']
+    for job, company in zip(jobs4, companies4):
+        st.write(f'{company}: {job}')
+
+# Choose a company and display title and pay
+st.header('Question 3: What companies are hiring?')
+st.subheader('Choose a company of interest to see if they are hiring:')
+st.write('Hint: popular companies in LA are: UCLA, USC, Snapchat, or Boeing')
+company = st.text_input(label ='Enter a company', value = 'ex: TikTok')
+
+is_in_dataframe = data['Company'].str.lower().str.contains(company.lower()).any()
+if is_in_dataframe:
+
+    st.write(f'Yes {company.upper()} is currently hiring!')
+    st.write(f'Here are the positions they are looking to fill:')
+else:
+    st.write(f'Sorry, there currently is no data on {company.upper()}\'s job postings')
